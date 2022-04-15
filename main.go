734e809527877f4f507bf08e8af3be9781b6225e
anupra12 <103685848+anupra12@users.noreply.github.com>
2022-04-15 14:30:46 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+
+    "database/sql"
+
+   
+
+    "fmt"
+
+    
+
+    "net/http"
+
+    _ "github.com/go-sql-driver/mysql"
+
+)
+
+var db *sql.DB
+
+var err error
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Fprintf(w, "Hello Home Page!")
+
+}
+
+
+
+type users struct {
+
+    ID       int
+
+    Name     string
+
+    Email    string
+
+    Password string
+
+    AccType  string
+
+}
+
+
+func connect() {
+
+    db, err = sql.Open("mysql", "root:1qaz2wsx@tcp(127.0.0.1:3306)/emp_data")
+
+    if err != nil {
+
+        fmt.Println(err.Error())
+
+    } else {
+
+        fmt.Println("successfully connected to database.")
+
+    }
+
+}
+
+
+
+
+
+func insert(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Fprintf(w, "isnert endpoint got hit!")
+
+  //  db.Exec("insert into users values(default, 'xxx','xxx@xxx','xxxpassword','user')") // for fixed //exection
+
+    name := "Harshal Rahangde"
+
+    id:=123
+     
+     salary:=456787
+
+    
+
+    //you can also use variables for a custom execution
+
+    db.Exec("insert into emp values(?,?,?)", id, name,salary)
+    fmt.Println("data inserted.")
+
+}
+
+func main() {
+
+    defer db.Close()
+
+    connect()
+
+    http.HandleFunc("/", home)
+
+    http.HandleFunc("/insert", insert)
+
+
+    fmt.Println("listening on port 8000")
+
+    http.ListenAndServe(":8000", nil)
+
+}
+
